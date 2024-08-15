6ed0297a2229844ce3353c9bcf00ccd0c02c7e74
madhumila <madhumithakp@gmail.com>
2024-08-15 14:02:31 +0530

    Update projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -12,7 +12,7 @@ const Projects = () => {
       <div className="w-[80%] pt-[1rem] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[2rem]">
            {projectsData.map(item=>(
         <div key={item.id}>
-            <a href="" target="_blank" rel="noopener noreferrer" >
+            <a href={item.live} target="_blank" rel="noopener noreferrer" >
 
             <div  className="transform cursor-pointer hover:-translate-y-6 transition-all duration-200 relative w-[100%] h-[200px] md:h-[300px]">
                 <Image
--- components/projectsData.ts
+++ components/projectsData.ts
@@ -1,41 +1,29 @@
 export const projectsData = [
   {
     id: 1,
-    name: "School-Management-System",
-    image: "/imagesport/projects/student.png",
-    technologies:
-      "NextJS, ReactJS, Mongodb, Next Auth, Typescript, Nodemailer, Tailwind CSS",
-    description: [
-      "Employed server-side rendering capabilities of Next JS for improved performance and faster page loads",
-      "Developed dynamic forms for easy and intuitive data entry for student information, academic grades, and attendance records",
-    ],
-    live:""
+    name: "Responsive Landing page for an Enterprise",
+    image: "/imagesport/projects/landing_page.png",
+    technologies: "ReactJS, Javascript, Tailwind CSS",
+    description: [],
+    github: "https://github.com/madhumila/habot_project",
+    live: "https://madhumila.github.io/habot_project/",
   },
   {
     id: 2,
-    name: "Chat-Application",
-    image: "/imagesport/projects/chatapp.png",
-    technologies:
-      "NextJS, ReactJS, Mongodb, Typescript, Tailwind CSS, SocketIO, Nodejs",
-    description: [
-      "Integrated Socket.IO for real-time bidirectional event-based communication, enabling instant messaging between users",
-      "Implemented a secure user authentication system allowing users to create accounts, log in, and maintain personalized chat profiles.",
-      "Database design and socket.io connection flow: https://app.eraser.io/workspace/uzrIRbfxWHCfaDx16gmO?origin=share",
-    ],
-    live:""
+    name: "Responsive Dashboard",
+    image: "/imagesport/projects/dashboard.png",
+    technologies: "ReactJS, Javascript, Tailwind CSS",
+    description: [],
+    github: "https://github.com/madhumila/FitPeo",
+    live: "https://madhumila.github.io/FitPeo/",
   },
   {
     id: 3,
-    name: "Ecommerce",
-    image: "/imagesport/projects/ecommerce.png",
-    technologies:
-    "NodeJS( Express), MongoDB, AWS, EJS",
-    description: [
-      "Integrated secure authentication mechanisms for admin and users, enhancing platform security.",
-      "Successfully integrated a third-party payment gateway (Razor Pay) for purchase orders.",
-      "Created comprehensive technical documentation, including Figma design, MongoDB database schemas, API documentation, and deployment procedures.",
-      
-    ],
-    live:""
+    name: "Local News and Weather Application",
+    image: "/imagesport/projects/news_and_weather.png",
+    technologies: "ReactJS, Javascript, Bootstrap",
+    description: [],
+    github: "https://github.com/madhumila/local-weather-news-app",
+    live: "https://madhumila.github.io/local-weather-news-app/",
   },
 ];
